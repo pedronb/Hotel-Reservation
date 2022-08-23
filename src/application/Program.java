30cc67a9a45eda490a14593bc39b035f0c5f3c71
Pedro Neves <pedronevesbezerra@gmail.com>
2022-08-23 15:56:11 -0300

    cria entidade, desenvolve regras

---
+++ src/application/Program.java
@@ -0,0 +1,50 @@
+package application;
+
+import entities.model.Reservation;
+import exceptions.DomainException;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        try {
+            System.out.print("Room number: ");
+            int roomNumber = sc.nextInt();
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            Date checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            Date checkOut = sdf.parse(sc.next());
+
+            Reservation r1 = new Reservation(roomNumber, checkIn, checkOut);
+            System.out.println(r1);
+            System.out.println();
+
+            System.out.println("Enter data to update the reservation:");
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            checkOut = sdf.parse(sc.next());
+            r1.updateDates(checkIn, checkOut);
+            System.out.println(r1);
+        }
+        catch (ParseException e) {
+            System.out.println("Error format date");
+        }
+        catch (DomainException e) {
+            System.out.println("Error in reservation: " + e.getMessage());
+        }
+        catch (RuntimeException e) {
+            System.out.println("Unexpected error");
+        }
+
+        sc.close();
+    }
+}
